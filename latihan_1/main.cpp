48d4c4a9e19d4a28df48b434b3765d2c41b7f99e
ayunisay <ayunisa1412@gmail.com>
2023-10-14 19:51:13 +0700

    menambahkan file latihan_1

---
+++ latihan_1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    cout << "Nama  : Ayunisa Yasmin\n" << endl;
+    cout << "NPM : 2310631170008\n" << endl;
+    cout << "Kelas : B - Informatika\n" << endl;
+    cout << "Cita - cita : UI/UX Designer\n" << endl;
+    cout << "Motivasi Masuk Informatika : Pertama karna kakak, kedua karna ingin melanjutkan dari SMK" << endl;
+    cout << "------------------------------------------------------------------------------------------" << endl;
+    return 0;
+}
